f8470757460e081277c71600b0f699192e3a6a23
greg1601 <greg1601@gmail.com>
2019-04-10 10:17:12 +0200

    modif route places en place

--- src/app/services/places.service.ts
+++ src/app/services/places.service.ts
@@ -11,6 +11,6 @@ export class PlacesService {
 
   constructor( private http:HttpClient) {}
 
-    sendPlaces = (item :Place) => this.http.post(this.url + "/places", item);
-    getAllPlace = () => this.http.get<Array<Place>>(this.url + "/places");
+    sendPlaces = (item :Place) => this.http.post(this.url + "/place", item);
+    getAllPlace = () => this.http.get<Array<Place>>(this.url + "/place");
 }
